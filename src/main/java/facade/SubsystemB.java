067311300ee69d91d03a44dc836a01b341ccc27c
xiangflight <xiangflight@foxmail.com>
2019-04-09 21:59:53 +0800

    add source of facade pattern

---
+++ src/main/java/facade/SubsystemB.java
@@ -0,0 +1,20 @@
+package facade;
+
+/**
+ * 子系统下的类B
+ *
+ * @author 赵翔 xiangflightATfoxmaildotcom
+ * @version facade
+ * @date 2019-04-09 21:50
+ */
+
+public class SubsystemB {
+
+    /**
+     * 处理业务逻辑
+     */
+    public void operate() {
+        // process
+    }
+
+}
